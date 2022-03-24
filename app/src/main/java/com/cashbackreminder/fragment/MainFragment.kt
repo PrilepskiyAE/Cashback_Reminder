1a2161ef10b332ae058ca1b94f67048b31c22a4e
Prilepskiy <woolf7754286@yandex.ru>
2022-03-24 16:55:59 +0700

    PI

--- app/src/main/java/com/cashbackreminder/fragment/MainFragment.kt
+++ app/src/main/java/com/cashbackreminder/fragment/MainFragment.kt
@@ -1,17 +1,22 @@
 package com.cashbackreminder.fragment
 
 
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import com.cashbackreminder.BaseFragment
 import com.cashbackreminder.FileDownloader
 import com.cashbackreminder.databinding.FragmentMainBinding
+import com.google.firebase.ktx.BuildConfig
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import io.reactivex.BackpressureStrategy
@@ -75,12 +80,11 @@ binding.button2.setOnClickListener {
         }, {
             Toast.makeText(requireContext(), "Complete Downloaded", Toast.LENGTH_SHORT).show()
             Log.d("TAG228", "Complete Downloaded ${targetFile.path}")
-            val promptInstall = Intent(Intent.ACTION_VIEW)
-                .setDataAndType(
-                    Uri.parse("file://${targetFile.path}"),
-                    "application/vnd.android.package-archive"
-                )
-            startActivity(promptInstall)
+
+          //  installAPK("")
+
+
+
         })
 
 
@@ -89,4 +93,52 @@ binding.button2.setOnClickListener {
 }
 
     }
+
+
+    fun installAPK(PATH:String) {
+        //val PATH = Environment.getExternalStorageDirectory().toString() + "/" + "apkname.apk"
+        val file = File(PATH)
+        if (file.exists()) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setDataAndType(
+                uriFromFile(
+                    requireContext().getApplicationContext(),
+                    File(PATH)
+                ), "application/vnd.android.package-archive"
+            )
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            try {
+                requireContext().startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+                Log.e("TAG", "Error in opening the file!")
+            }
+        } else {
+            Toast.makeText(
+               requireContext(),
+                "installing",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    fun uriFromFile(context: Context, file: File?): Uri? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            FileProvider.getUriForFile(
+                context, BuildConfig.APPLICATION_ID.toString() + ".provider",
+                file!!
+            )
+        } else {
+            Uri.fromFile(file)
+        }
+
+        val intent = Intent(Intent.ACTION_INSTALL_PACKAGE)
+        intent.data = Uri.fromFile(File(pathToApk))
+        startActivity(intent)
+    }
+
+
+
+
 }
